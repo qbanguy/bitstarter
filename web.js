703a8350d7e2ff5e4f876c0022cc3014e03ac162
Adrian Sanchez del C <qbanguy7@gmail.com>
2013-07-22 21:20:14 -0700

    edited web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.readFile(__dirname + '/index.html', 'utf8', function(err, text){
+		 response.send(text);
+	});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening localhost:%s on " + port, port);
 });
